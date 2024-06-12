76d21af1773c32f326aecc76c8b4e8a89d11b86c
Humna Khan <112195058+Humna-Khan@users.noreply.github.com>
2024-06-12 23:49:03 +0500

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,4 @@
+tempCelsius = float(input("Enter temperature in Celsius: "))
+tempfahrenheit = (tempCelsius * 9/5) + 32
+temp_kelvin = tempCelsius + 273.15
+print(f"The temperature {tempCelsius} degrees Celsius is equal to {tempfahrenheit} degrees Fahrenheit and {temp_kelvin} Kelvin.")
